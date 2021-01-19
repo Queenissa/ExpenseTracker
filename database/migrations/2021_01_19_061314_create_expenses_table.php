27cc25c123b36c548ae496950bc238bddc463e10
2ndyrGroupC <2ndyrGroupC@pn.local>
2021-01-19 15:24:57 +0800

    quency updates

--- database/migrations/2021_01_19_061314_create_expenses_table.php
+++ database/migrations/2021_01_19_061314_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->reference('id')->on('categories');
+            $table->double('expense_amount',9,2);
+            $table->date('expense_date');
             $table->timestamps();
         });
     }
@@ -32,6 +30,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses');
+        
     }
 }
